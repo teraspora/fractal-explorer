db592fa5e9126d4b40b13ffd260562d5bb8336ed
John Lynch <johnlynch.32768@gmail.com>
2018-08-18 14:44:34 +0100

    Refactored to allow variable subsets of Complex plane

--- scripts/webfractals.js
+++ scripts/webfractals.js
@@ -1,7 +1,17 @@
 const W = 768;
 const H = 768;
-const xSpan = 4.0;
-const ySpan = 4.0;
+
+
+var zMin = {re: -2, im: -2};
+var zMax = {re: 2, im: 2};
+var xSpan = zMax.re - zMin.re;
+var ySpan = zMax.im - zMin.im;
+
+
+const colours = ["#000000", "#ffdcb0"  ,  "#000000",  "#c33664" ,  "#000000",  "#56cbff" ,  "#000000",  
+         "#ff7ccd" ,  "#000000",  "#f93457" ,  "#000000",  "#305cff" ,                  
+         "#000000",  "#008aff" ,  "#000000"];
+
 const canv = document.getElementById("root-canvas");
 canv.width = W;
 canv.height = H;
@@ -10,25 +20,33 @@ var imgData = gc.createImageData(W, H);
 
 var isJulia = false;
 var juliaPoint = {re: 0, im: 0};
+var maxIterations = 256;
 
 draw();
 
 function draw() {
-    for (i = 0; i < imgData.data.length; i += 4) {
+    var colourMappingFactor = (colours.length - 2) / maxIterations; 
+    for (i = 0; i < imgData.data.length; i += 4) {      // image data has 4 entries (RGBA) for each pixel, scanning L to R for each line
 
         var pixelNum = Math.floor(i / 4);
+        // Get x and y coords of pixel
         var px = Math.floor(pixelNum % W)
         var py = Math.floor(pixelNum / W);
-
-        imgData.data[i] = 8;
-        imgData.data[i+1] = 0;
-        imgData.data[i+2] = iterate(f, pixelToComplex(px, py));
+        var iterationCount = iterate(f, pixelToComplex(px, py));
+        var colourIndex = iterationCount * colourMappingFactor;
+        var firstColourIndex = Math.floor(colourIndex);
+        var interpolationFactor = colourIndex % 1;
+        var finalColour = interpolateColour(hexrgb(colours[firstColourIndex]), hexrgb(colours[firstColourIndex + 1]), interpolationFactor);
+        imgData.data[i] = finalColour[0];
+        imgData.data[i+1] = finalColour[1];
+        imgData.data[i+2] = finalColour[2];
         imgData.data[i+3] = 255;
+        if (px === W / 2) console.log(iterationCount);
     }
     gc.putImageData(imgData, 16, 16);
 }
 
-function f(z, c) {		// standard Mandelbrot
+function f(z, c) {		// standard Mandelbrot / Julia of "z -> z squared plus c"
     var x = z.re;
     var y = z.im;
     var newX = x * x - y * y + c.re;
@@ -39,7 +57,7 @@ function f(z, c) {		// standard Mandelbrot
 function iterate(func, z) {
     var numIterations = 0;
     var z0 = isJulia ? juliaPoint : z;
-    while (numIterations < 256 && cmod2(z) < 16) {
+    while (numIterations < maxIterations && cmod2(z) < 32) {
         numIterations++;
         z = func(z, z0);
     }
@@ -56,17 +74,8 @@ function pixelToComplex(px, py) {
     return {re: px / (W / xSpan) - xSpan / 2, im: ySpan / 2 - py / (H / ySpan)}
 }
 
-// function clickHandler(e) {
-//     if (isJulia) return;
-//     e = e || window.event;
-//     var x = e.pageX;
-//     var y = e.pageY;
-//     juliaPoint = pixelToComplex(x, y);
-//     console.log("Clicked at (" + x + ", " + y + ")");
-//     isJulia = true;
-//     draw();
-// }
-
+// MOUSE CLICK HANDLING
+// ====================
 function clickHandler(e) {
     if (isJulia) {
         isJulia = false;
@@ -95,3 +104,32 @@ function getMousePos(c, e) {       // got from https://codepen.io/chrisjaime/pen
 // attach handler to click event
 if (document.attachEvent) document.attachEvent('onclick', clickHandler);
 else document.addEventListener('click', clickHandler);
+
+// ====================
+
+// COLOUR STUFF
+// ====================
+// Th following code I stole from https://codepen.io/njmcode/pen/axoyD and slightly modified...
+
+// Interpolates two [r,g,b] colors and returns an [r,g,b] of the result
+// Taken from the awesome ROT.js roguelike dev library at
+// https://github.com/ondras/rot.js
+
+function interpolateColour(colour1, colour2, factor) {
+  if (arguments.length < 3) { factor = 0.5; }
+  var rgb = colour1.slice();
+  for (var i=0;i<3;i++) {
+    rgb[i] = Math.round(rgb[i] + factor*(colour2[i]-colour1[i]));
+  }
+  return rgb;
+};
+
+// Converts a #ffffff hex string into an [r,g,b] array
+function hexrgb(hexColour) {
+    var rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexColour);  // pull out 3 pairs of hex digits
+    return rgb ? [
+        parseInt(rgb[0], 16),
+        parseInt(rgb[1], 16),
+        parseInt(rgb[2], 16)
+    ] : null;
+};
