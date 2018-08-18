a225d770299ea3d34937b9cefa892afb2be9de71
John Lynch <johnlynch.32768@gmail.com>
2018-08-18 05:14:25 +0100

    Added showing the Julia Set for any point clicked

--- scripts/webfractals.js
+++ scripts/webfractals.js
@@ -1,24 +1,17 @@
 const W = 768;
 const H = 768;
-const xSpan = 4;
-const ySpan = 4;
-var c = document.getElementById("root-canvas");
-c.width = W;
-c.height = H;
-var ctx = c.getContext("2d");
+const xSpan = 4.0;
+const ySpan = 4.0;
+const canv = document.getElementById("root-canvas");
+canv.width = W;
+canv.height = H;
+const gc = canv.getContext("2d");
+var imgData = gc.createImageData(W, H);
 
-var imgData = ctx.createImageData(W, H);
+var isJulia = false;
+var juliaPoint = {re: 0, im: 0};
 
 draw();
-
-// function handler(e) {
-//     e = e || window.event;
-
-//     var pX = e.pageX;
-//     var pY = e.pageY;
-
-//     draw(pX, pY);
-// }
 
 function draw() {
     for (i = 0; i < imgData.data.length; i += 4) {
@@ -27,12 +20,12 @@ function draw() {
         var px = Math.floor(pixelNum % W)
         var py = Math.floor(pixelNum / W);
 
-        imgData.data[i] = 16;
+        imgData.data[i] = 8;
         imgData.data[i+1] = 0;
         imgData.data[i+2] = iterate(f, pixelToComplex(px, py));
         imgData.data[i+3] = 255;
     }
-    ctx.putImageData(imgData, 16, 16);
+    gc.putImageData(imgData, 16, 16);
 }
 
 function f(z, c) {		// standard Mandelbrot
@@ -45,7 +38,7 @@ function f(z, c) {		// standard Mandelbrot
 
 function iterate(func, z) {
     var numIterations = 0;
-    var z0 = z;
+    var z0 = isJulia ? juliaPoint : z;
     while (numIterations < 256 && cmod2(z) < 16) {
         numIterations++;
         z = func(z, z0);
@@ -60,9 +53,45 @@ function cmod2(z) {
 }
 
 function pixelToComplex(px, py) {
-    return {re: px / (W / 4) - xSpan / 2, im: ySpan / 2 - py / (H / 4)}
+    return {re: px / (W / xSpan) - xSpan / 2, im: ySpan / 2 - py / (H / ySpan)}
 }
 
+// function clickHandler(e) {
+//     if (isJulia) return;
+//     e = e || window.event;
+//     var x = e.pageX;
+//     var y = e.pageY;
+//     juliaPoint = pixelToComplex(x, y);
+//     console.log("Clicked at (" + x + ", " + y + ")");
+//     isJulia = true;
+//     draw();
+// }
+
+function clickHandler(e) {
+    if (isJulia) {
+        isJulia = false;
+        draw();
+        return;
+    }
+    e = e || window.event;
+    var pixel = getMousePos(canv, e);
+    var x = pixel.x;
+    var y = pixel.y;
+    juliaPoint = pixelToComplex(x, y);
+    console.log("Clicked at (" + x + ", " + y + ")");
+    isJulia = true;
+    draw();
+}
+
+function getMousePos(c, e) {       // got from https://codepen.io/chrisjaime/pen/lcEpn; takes a canvas and an event (mouse-click)
+    var bounds = c.getBoundingClientRect();
+    return {
+        x: e.clientX - bounds.left,
+        y: e.clientY - bounds.top
+    };
+}
+
+
 // attach handler to click event
-// if (document.attachEvent) document.attachEvent('onclick', handler);
-// else document.addEventListener('click', handler);
+if (document.attachEvent) document.attachEvent('onclick', clickHandler);
+else document.addEventListener('click', clickHandler);
